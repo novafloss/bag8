3b5a9ab365a40b177541d40ca7524cb4b275d2cb
Florent <florent.pigout@novapost.fr>
2015-07-01 12:52:45 +0200

    Fixes non working dev_command with busybox.
    Requires real ENTRYPOINT which looks like little bit tricky with busybox.

--- bag8/tests/test_yaml.py
+++ bag8/tests/test_yaml.py
@@ -37,7 +37,6 @@ def test_data():
     project = Project('busybox', develop=True)
     assert Yaml(project).data == {
         'busybox': {
-            'command': 'sh',
             'dockerfile': os.path.join(project.build_path, 'Dockerfile'),
             'environment': [
                 'DNSDOCK_ALIAS=busybox.docker',
@@ -98,7 +97,6 @@ def test_service_dicts():
     assert sorted(Yaml(project).service_dicts) == sorted([
         {
             'name': 'busybox',
-            'command': 'sh',
             'dockerfile': os.path.join(project.build_path, 'Dockerfile'),
             'environment': [
                 'DNSDOCK_ALIAS=busybox.docker',
